e0f02e74158c1fb377663de47b175a8f1d10d41a
larasobral <50533844+larasobral@users.noreply.github.com>
2023-10-12 11:28:14 -0300

    Update divbyN.sv
    transformando a matriz para vetor

--- divbyN.sv
+++ divbyN.sv
@@ -1,11 +1,8 @@
-//parameter BIT_INT = 16;
-//parameter BIT_FRAC = 16;
-//parameter W = BIT_INT+BIT_FRAC;
 `include "invN.sv"
 
 module divbyN  #(parameter N=16, parameter W=16, parameter BIT_FRAC=1
-		)(input logic signed [W:0] x[N][1:0],
-                 output logic signed [W:0] y[N][1:0]);
+		)(input logic signed [W:0] x[2*N],
+                 output logic signed [W:0] y[2*N]);
 	  
 	logic signed [W:0] inv_N;
 	invN #(.N(N), .W(W), .BIT_FRAC(BIT_FRAC)) m_invN (
@@ -22,14 +19,14 @@ module divbyN  #(parameter N=16, parameter W=16, parameter BIT_FRAC=1
 	generate 
 		if(c) begin
 			for(k=0; k<N; k++)begin
-				assign y[k][0] = x[k][0]>>>p;
-				assign y[k][1] = x[k][1]>>>p;
+				assign y[k] = x[k]>>>p;
+				assign y[k + N] = x[k + N]>>>p;
 			end
 		end  
 		else begin  
 			for(k=0; k<N; k++)begin
-				assign y[k][0] = round(x[k][0]*inv_N);
-				assign y[k][1] = round(x[k][1]*inv_N);
+				assign y[k] = round(x[k]*inv_N);
+				assign y[k + N] = round(x[k + N]*inv_N);
 			end
 		end 
 	endgenerate
@@ -43,6 +40,7 @@ module divbyN  #(parameter N=16, parameter W=16, parameter BIT_FRAC=1
 	endfunction: isPowerOfTwo
   
 endmodule: divbyN
+
 
   
 
