c318843fbc2065fe0b1d0525e2fd4b5fd62753f7
larasobral <50533844+larasobral@users.noreply.github.com>
2023-10-12 11:03:27 -0300

    Update computeMatrix.sv
    transformação da matriz em vetor. inclusão rom

--- computeMatrix.sv
+++ computeMatrix.sv
@@ -1,50 +1,39 @@
-`include "LUT.sv"
+`include "rom.sv"
 
 module computeMatrix #(
     parameter N = 16,
     parameter W = 16 
 )(
-    input logic signed [W-1:0] x[N][1:0],
-    output logic signed [W + N - 1:0] X[N][1:0]
+    input logic signed [W:0] x[2*N],
+    output logic signed [W + N - 1:0] X[2*N]
 );
-    //genvar k, j;
-    
-    //int k, j; 
-	
-    localparam theta = -2 * $acos(-1) / N;
-    
-    logic signed [W - 1:0] angle_values[N][N];
-    logic signed [W + N - 1:0] cos_values[N][N];
-    logic signed [W + N - 1:0] sin_values[N][N];
+
+    logic [W-1:0] sin[N];
+    logic [W-1:0] cos[N];
+    logic [6:0] address;
+
+
+    rom #(.W(W), .N(N))ROM_instance(
+    .address(address), // Entrada de ângulo de 0 a 90 graus (0 a 90 em ponto fixo)
+    .sin(sin), // Saída do seno
+    .cos(cos)); // Saída do cosseno
+
+
+   // Inicialização dos valores de sin e cos (um único cálculo no início)
 
     // Computation of the Fourier matrix
-    generate
-        for (genvar k = 0; k < N; k++) begin
-            for (genvar j = 0; j < N; j++) begin
-                LUT #(W, N) lut_cos (
-                    .sine_or_cosine(1'b1), 
-                    .angle(angle_values[k][j]), 
-                    .value(cos_values[k][j])
-                );
-
-                LUT #(W, N) lut_sin (
-                    .sine_or_cosine(1'b0), 
-                    .angle(angle_values[k][j]), 
-                    .value(sin_values[k][j])
-                );
-            end
-        end
-    endgenerate
-	always_comb begin
-		for (int k = 0; k < N; k++) begin
-	    		X[k][1:0] = {0,0};
-            		for (int j = 0; j < N; j++) begin
-				X[k][0] = X[k][0]+(cos_values[k][j]*x[j][0])-(sin_values[k][j]*x[j][1]);
-        			X[k][1] = X[k][1]+(cos_values[k][j]*x[j][1])+(sin_values[k][j]*x[j][0]);
-
-            		end
+	generate
+		for (genvar k = 0; k < N; k++) begin
+	    		assign X[0] = 0;
+			assign X[N] = 0;
+		//n address = k;  
+			if (k<2*N)begin 
+				assign X[k] = X[0] + (cos[k]*x[k]) - (sin[k]*x[N+k]);
+			end
+			else begin 
+				assign X[N+k] = X[N] + (cos[k]*x[N+k]) + (sin[k]*x[k]);
+			end
         	end
-	end
+	endgenerate
+
 endmodule: computeMatrix 
-
-
