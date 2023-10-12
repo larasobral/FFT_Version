ac90b8d436d8edea77a4fe277e8b8907b78f998a
larasobral <50533844+larasobral@users.noreply.github.com>
2023-10-12 11:30:38 -0300

    Update swap.sv
    matriz para vetor

--- swap.sv
+++ swap.sv
@@ -1,12 +1,12 @@
-module swap #(parameter N=4, parameter W=4)
-             (input logic signed [W:0] x[N][1:0],
-              output logic signed [W:0] y[N][1:0]);
+module swap #(parameter N=16, parameter W=16)
+             (input logic signed [W:0] x[2*N],
+              output logic signed [W:0] y[2*N]);
 
 	genvar k;
 	generate 
 		for(k=0; k<N; k++)begin
-			assign y[k][0] = x[k][1];
-			assign y[k][1] = x[k][0];
+			assign y[k] = x[k+N];
+			assign y[k+N] = x[k];
 		end
 	endgenerate
 
