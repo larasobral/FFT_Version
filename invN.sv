aaaa6c20ccfc22220293b1ce172a326be01f0554
larasobral <50533844+larasobral@users.noreply.github.com>
2023-11-09 09:44:56 -0300

    Update invN.sv

--- invN.sv
+++ invN.sv
@@ -1,22 +1,18 @@
-module invN #(parameter N=16, 
-	      parameter W=16, 
-	      parameter BIT_FRAC=1
-	)
-	(output logic signed [W:0] y
-	);
+module invN #(parameter N=16, parameter W=16, parameter BIT_FRAC=1)           
+             (output logic signed [W:0] y);
   
-	logic [W:0] A;
-	logic [W:0] T;
-	always_comb begin
-		A = 0;
-		T=1 << (BIT_FRAC);
-		for(int k = (BIT_FRAC); k >= 0; k--)begin 
-			if(N<<k <= T)begin
-				A = A+(1<<k);
-				T = T-(N<<k);
-			end
-			y = A;
-		end
-	end
+  logic [W:0] A;
+  logic [W:0] T;
+  always_comb begin
+    A = 0;
+    T=1<<BIT_FRAC;
+    for(int k=BIT_FRAC; k>=0; k--) begin
+      if(N<<k <= T)begin
+        A = A+(1<<k);
+        T = T-(N<<k);
+      end
+    y = A;    
+    end
+  end
   
 endmodule: invN
