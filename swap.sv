9154107f16fbc1eb8a6f130da89661d32cd4b1f4
larasobral <50533844+larasobral@users.noreply.github.com>
2023-11-09 09:45:19 -0300

    Update swap.sv

--- swap.sv
+++ swap.sv
@@ -1,12 +1,12 @@
 module swap #(parameter N=16, parameter W=16)
-             (input logic signed [W:0] x[2*N],
-              output logic signed [W:0] y[2*N]);
+             (input logic signed [W:0] x[N][1:0],
+              output logic signed [W:0] y[N][1:0]);
 
-	genvar k;
+
 	generate 
-		for(k=0; k<N; k++)begin
-			assign y[k] = x[k+N];
-			assign y[k+N] = x[k];
+		for(genvar k=0; k<N; k++)begin
+			assign y[k][0] = x[k][1];
+			assign y[k][1] = x[k][0];
 		end
 	endgenerate
 
